85c9f241a15791e624b38a26aeb31ff7967c4b78
beekill <nguyenmbquan95@gmail.com>
2023-06-30 07:39:42 -0700

    Implement naive collisions solver

--- src/BallWorld.hs
+++ src/BallWorld.hs
@@ -3,6 +3,7 @@ module BallWorld where
 import Ball
 import Container
 import Graphics.Gloss
+import Object (RigidShape (solveCollision))
 import Renderer
 import VerletObject
 import World
@@ -13,14 +14,30 @@ data BallWorld = BallWorld
   }
 
 instance World BallWorld where
-  updateWorld t w = w {balls = objs}
+  updateWorld t w = w {balls = update w}
     where
       moveBalls world = map (move t) (balls world)
       bounceOffContainer = map $ bounceOff $ container w
 
-      objs = bounceOffContainer $ moveBalls w
+      update = solveCollisions . bounceOffContainer . moveBalls
 
 instance Renderable BallWorld where
   render w = Pictures p
     where
       p = render (container w) : map render (balls w)
+
+-- FIXME: move this to CollisionsSolvers module
+-- and abstract this to work with RigidShape;
+-- and better naming.
+solveCollisions :: [Ball] -> [Ball]
+solveCollisions [] = []
+solveCollisions (h : rest) = hNew : solveCollisions restNew
+  where
+    (hNew, restNew) = individuallySolveCollisions h rest
+
+individuallySolveCollisions :: Ball -> [Ball] -> (Ball, [Ball])
+individuallySolveCollisions x [] = (x, [])
+individuallySolveCollisions x (h : rest) = (x2, hNew : restNew)
+  where
+    (x1, hNew) = solveCollision x h
+    (x2, restNew) = individuallySolveCollisions x1 rest
---
+++ src/CollisionsSolvers.hs
@@ -0,0 +1,1 @@
+module CollisionsSolvers where
