a62da0b2edae6cfaca8bdb408f1d6c25fa872d49
beekill <nguyenmbquan95@gmail.com>
2023-07-03 12:23:04 -0700

    Improve performance of naive collisions solver by forcing evaluation using `seq`.

--- src/CollisionsSolvers.hs
+++ src/CollisionsSolvers.hs
@@ -4,12 +4,12 @@ import Object
 
 solveCollisionsNaively :: (RigidObject a) => [a] -> [a]
 solveCollisionsNaively [] = []
-solveCollisionsNaively (x : xs) = xNew : solveCollisionsNaively xsNew
+solveCollisionsNaively (x : xs) = xNew `seq` xNew : solveCollisionsNaively xsNew
   where
     solveCollisions y [] = (y, [])
-    solveCollisions y (h : rest) = (y2, h1 : rest2)
+    solveCollisions y (h : rest) = y2 `seq` (y2, h1 : rest2)
       where
         (y1, h1) = solveCollision y h
-        (y2, rest2) = solveCollisions y1 rest
+        (y2, rest2) = y1 `seq` solveCollisions y1 rest
 
     (xNew, xsNew) = solveCollisions x xs
